863e403a01fe5c7413b3a5140fe87d4708cb9ac4
leviskip12 <levisshiko01@gmail.com>
2025-07-26 15:21:23 -0700

    Add files via upload

---
+++ 2D array.cpp
@@ -0,0 +1,25 @@
+/*
+Author:Levis kipruto chumbaAuthor
+REG :BSE-01-0036/2025
+Desccription: program to demonstrate 2D array
+Date 20-07/2025*/
+
+#include <iostream>
+using namespace std;
+#include <iostream>
+using namespace std;
+//main 
+int main() {
+	//initializing
+    int matrix[2][3] = {
+        {5, 10, 15},
+        {20, 25, 30}
+    };
+
+    for(int i=0;i<2;i++){
+     for(int j=0;j<3;j++)
+     	cout<< matrix [i][j]<<"\t";
+     cout<<endl;
+	}
+    return 0;
+}
